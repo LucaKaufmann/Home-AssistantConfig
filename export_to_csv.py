cab9b3c74027e50f96354b2dfb14ce2a20fa96ab
Luca <lucakaufmann92@gmail.com>
2019-05-27 13:21:18 +0300

    updating hue sensors, updating sonos scripts, adding new camera scripts

--- export_to_csv.py
+++ export_to_csv.py
@@ -8,23 +8,23 @@ parser.add_option("-r", "--rssi", dest="rssi",
 parser.add_option("-s", "--signal", dest="signal",
                   help="signal", action="store", type="int")
 parser.add_option("-a", "--ap", dest="ap",
-                  help="ap mac", action="store", type="int")
-parser.add_option("-d", "--downstairs", dest="downstairs",
-                  help="downstairs", action="store", type="int")
+                  help="ap mac", action="store", type="string")
+parser.add_option("-l", "--location", dest="location",
+                  help="location", action="store", type="string")
 
 (options, args) = parser.parse_args()
 rssi = options.rssi
 signal = options.signal
 ap = options.ap
-downstairs = options.downstairs
+location = options.location
 
-values = Path("/config/export/values.csv")
+values = Path("/config/export/wifi_values.csv")
 if values.is_file():
-    with open('/config/export/values.csv', 'a') as newFile:
+    with open('/config/export/wifi_values.csv', 'a') as newFile:
         newFileWriter = csv.writer(newFile)
-        newFileWriter.writerow([rssi,signal,ap,downstairs])
+        newFileWriter.writerow([rssi,signal,ap,location])
 else:
-    with open('/config/export/values.csv','w') as newFile:
+    with open('/config/export/wifi_values.csv','w') as newFile:
         newFileWriter = csv.writer(newFile)
-        newFileWriter.writerow(['rssi','signal','mac','downstairs'])
-        newFileWriter.writerow([rssi,signal,ap,downstairs])
+        newFileWriter.writerow(['rssi','signal','mac','location'])
+        newFileWriter.writerow([rssi,signal,ap,location])
