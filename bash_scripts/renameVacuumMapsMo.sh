ce39b3867dae13c3e5abb2691fb2f78f265ac663
Luca Kaufmann <lucakaufmann92@gmail.com>
2019-11-27 09:21:49 +0200

    Adding heating servo, esphome, updated vacuum scripts, new light automation, updated devices view

--- bash_scripts/renameVacuumMapsMo.sh
+++ bash_scripts/renameVacuumMapsMo.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-filesize=$(wc -c < /images/vacuum_maps_mo/latest.png)
-filesizeSnapshot=$(wc -c < /images/vacuum_maps_mo/snapshot1.png)
+filesize=$(wc -c < /images/vacuum_maps_mo/latest.jpg)
+filesizeSnapshot=$(wc -c < /images/vacuum_maps_mo/snapshot1.jpg)
 minimumsize=0
 if [ $filesize -gt $minimumsize ]; then
     if [ $filesize -eq $filesizeSnapshot ]; then
         exit
     fi
-    cp /images/vacuum_maps_mo/snapshot5.png /images/vacuum_maps_mo/snapshot5_"$(date -r /images/vacuum_maps_mo/snapshot5.png +"%Y%m%d_%H%M%S").png"
-    mv /images/vacuum_maps_mo/snapshot4.png /images/vacuum_maps_mo/snapshot5.png
-    mv /images/vacuum_maps_mo/snapshot3.png /images/vacuum_maps_mo/snapshot4.png
-    mv /images/vacuum_maps_mo/snapshot2.png /images/vacuum_maps_mo/snapshot3.png
-    mv /images/vacuum_maps_mo/snapshot1.png /images/vacuum_maps_mo/snapshot2.png
-    cp /images/vacuum_maps_mo/latest.png /images/vacuum_maps_mo/snapshot1.png
+    cp /images/vacuum_maps_mo/snapshot5.jpg /images/vacuum_maps_mo/snapshot5_"$(date -r /images/vacuum_maps_mo/snapshot5.jpg +"%Y%m%d_%H%M%S").jpg"
+    mv /images/vacuum_maps_mo/snapshot4.jpg /images/vacuum_maps_mo/snapshot5.jpg
+    mv /images/vacuum_maps_mo/snapshot3.jpg /images/vacuum_maps_mo/snapshot4.jpg
+    mv /images/vacuum_maps_mo/snapshot2.jpg /images/vacuum_maps_mo/snapshot3.jpg
+    mv /images/vacuum_maps_mo/snapshot1.jpg /images/vacuum_maps_mo/snapshot2.jpg
+    cp /images/vacuum_maps_mo/latest.jpg /images/vacuum_maps_mo/snapshot1.jpg
 fi
