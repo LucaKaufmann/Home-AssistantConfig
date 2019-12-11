c5bdd3c065b7da608752b56137b9f58b30b181e7
Luca Kaufmann <lucakaufmann92@gmail.com>
2019-12-11 19:58:47 +0200

    adding new rename scripts, removing unused sensors, adding new dashboards, updating scripts

--- bash_scripts/renameCar.sh
+++ bash_scripts/renameCar.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cp /images/raspberry/latest.jpg /images/cars/car_"$(date -r /images/vacuum_maps/snapshot5.png +"%Y%m%d_%H%M%S").png"
+cp /images/raspberry/latest.jpg /images/cars/car_"$(date -r /images/raspberry/latest.jpg +"%Y%m%d_%H%M%S").jpg"
--- bash_scripts/renamePerson.sh
+++ bash_scripts/renamePerson.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cp /images/raspberry/latest.jpg /images/persons/person_"$(date -r /images/vacuum_maps/snapshot5.png +"%Y%m%d_%H%M%S").png"
+cp /images/raspberry/latest.jpg /images/persons/person_"$(date -r /images/raspberry/latest.jpg +"%Y%m%d_%H%M%S").jpg"
