7c22ced847c694b104e66dfda31567ac2cd0fa1f
Luca Kaufmann <lucakaufmann92@gmail.com>
2020-01-11 15:31:52 +0200

    updating dashboards, fixing car renaming scripts

--- bash_scripts/renameCar.sh
+++ bash_scripts/renameCar.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+cp /images/raspberry/latest.jpg /images/cars/car_latest.jpg
 cp /images/raspberry/latest.jpg /images/cars/car_"$(date -r /images/raspberry/latest.jpg +"%Y%m%d_%H%M%S").jpg"
